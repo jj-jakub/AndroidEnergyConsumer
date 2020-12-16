75cfef8b01cf65a0d3cbe3eac9ad88aeb62612e5
jj <>
2020-12-16 23:02:35 +0100

    Create new file for logs if current one does not exists

--- app/src/main/java/com/jj/androidenergyconsumer/utils/LogSaver.kt
+++ app/src/main/java/com/jj/androidenergyconsumer/utils/LogSaver.kt
@@ -9,11 +9,12 @@ object LogSaver {
     private const val LOGS_FILE_NAME = "LOGS_FILE.txt"
 
     private var bufferedFileWriter: BufferedWriter? = null
+    private var currentFile: File? = null
 
     private fun createWriter() {
         try {
-            bufferedFileWriter =
-                BufferedWriter(FileWriter(File(Environment.getExternalStorageDirectory(), LOGS_FILE_NAME), true))
+            val file = File(Environment.getExternalStorageDirectory(), LOGS_FILE_NAME)
+            bufferedFileWriter = BufferedWriter(FileWriter(file, true))
         } catch (e: FileNotFoundException) {
             Log.e(tag, "Lack of permission", e)
         }
@@ -21,7 +22,7 @@ object LogSaver {
 
     @Synchronized
     fun saveLog(tag: String?, message: String?) {
-        if (bufferedFileWriter == null) createWriter()
+        if (currentFile?.exists() != true || bufferedFileWriter == null) createWriter()
         try {
             bufferedFileWriter?.append("${getDateStringWithMillis()} $tag: $message\n")
             bufferedFileWriter?.flush()
