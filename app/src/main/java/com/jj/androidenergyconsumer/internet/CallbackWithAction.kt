48686c15ac2c5c1f0c1aeab588bd465bbc2ee076
jj <>
2021-02-10 08:49:09 +0100

    Create CallbackWithActionTest

--- app/src/main/java/com/jj/androidenergyconsumer/internet/CallbackWithAction.kt
+++ app/src/main/java/com/jj/androidenergyconsumer/internet/CallbackWithAction.kt
@@ -7,16 +7,19 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class CallbackWithAction(private val onCallFinished: (result: String) -> Unit) : Callback<ResponseBody> {
+class CallbackWithAction(private val onCallFinishedCallback: (result: String) -> Unit) : Callback<ResponseBody> {
     override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
         val message = "onFailure, ${t.localizedMessage}"
-        logAndPingServer(message, tag)
         onCallFinished(message)
     }
 
     override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
         val message = "onResponse, ${response.code()}"
-        logAndPingServer(message, tag)
         onCallFinished(message)
+    }
+
+    private fun onCallFinished(message: String) {
+        logAndPingServer(message, tag)
+        onCallFinishedCallback(message)
     }
 }
