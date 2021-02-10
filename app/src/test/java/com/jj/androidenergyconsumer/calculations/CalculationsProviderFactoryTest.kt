45b779ba5a0dd58b182b811bcbcbc0c82e11ddc0
jj <>
2021-02-10 08:29:09 +0100

    Create parameterized test for CalculationsProviderFactory

---
+++ app/src/test/java/com/jj/androidenergyconsumer/calculations/CalculationsProviderFactoryTest.kt
@@ -0,0 +1,47 @@
+package com.jj.androidenergyconsumer.calculations
+
+import android.content.Intent
+import com.jj.androidenergyconsumer.services.CalculationsService
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
+import org.mockito.Mockito
+
+class CalculationsProviderFactoryTest {
+
+    companion object {
+        @JvmStatic
+        private fun calculationsTypes() = CalculationsType.values().toList()
+
+        @JvmStatic
+        private fun orderedExpectedCalculationsProvidersTypes() = listOf(
+                AdditionCalculationsProvider::class.java,
+                MultiplicationCalculationsProvider::class.java)
+
+        @JvmStatic
+        fun calculationsTypesAndExpectedResults() =
+            listsToPairs(calculationsTypes(), orderedExpectedCalculationsProvidersTypes())
+
+        private fun <K, V> listsToPairs(list1: List<K>, list2: List<V?>): List<Pair<K, V?>> {
+            val finalMap = HashMap<K, V?>()
+            list1.forEachIndexed { index, key ->
+                val value = list2.getOrNull(index)
+                finalMap[key] = value
+            }
+            return finalMap.toList()
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource("calculationsTypesAndExpectedResults")
+    fun `factory should return addition calculations provider`(
+            typeAndResult: Pair<CalculationsType, Class<out CalculationsProvider>>) {
+        val intentMock = Mockito.mock(Intent::class.java)
+        val calculationsCallbackMock = Mockito.mock(CalculationsCallback::class.java)
+
+        Mockito.`when`(intentMock.getSerializableExtra(CalculationsService.CALCULATIONS_TYPE_EXTRA))
+            .thenReturn(typeAndResult.first)
+        assertTrue(CalculationsProviderFactory.createCalculationsProvider(intentMock,
+                calculationsCallbackMock)::class.java == typeAndResult.second)
+    }
+}
