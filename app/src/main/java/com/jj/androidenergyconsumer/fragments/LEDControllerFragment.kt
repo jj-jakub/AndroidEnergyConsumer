4f7decf8354109a79eafcb38b76cb0148c367228
jj <>
2021-03-02 10:39:45 +0100

    Implement LEDControllerFragment
    .

--- app/src/main/java/com/jj/androidenergyconsumer/fragments/LEDControllerFragment.kt
+++ app/src/main/java/com/jj/androidenergyconsumer/fragments/LEDControllerFragment.kt
@@ -2,20 +2,93 @@ package com.jj.androidenergyconsumer.fragments
 
 import android.content.ServiceConnection
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.jj.androidenergyconsumer.databinding.FragmentLedControllerBinding
+import com.jj.androidenergyconsumer.utils.showShortToast
+import com.jj.androidenergyconsumer.viewmodels.LedControllerViewModel
 
-class LEDControllerFragment: BaseLauncherFragment() {
+enum class AvailableLedColors {
+    RED, GREEN, BLUE, YELLOW, WHITE, PURPLE, CYAN
+}
 
+class LEDControllerFragment : BaseLauncherFragment() {
+
+    override val activityTitle: String = "LED Controller"
     override val serviceConnection: ServiceConnection
         get() = TODO("Not yet implemented")
 
     private lateinit var fragmentLedControllerBinding: FragmentLedControllerBinding
 
+    private val ledControllerViewModel: LedControllerViewModel by viewModels()
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         fragmentLedControllerBinding = FragmentLedControllerBinding.inflate(inflater, container, false)
         return fragmentLedControllerBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupButtonListeners()
+    }
+
+    private fun setupButtonListeners() {
+        with(fragmentLedControllerBinding) {
+            redLedButton.setOnClickListener { switchLeds(AvailableLedColors.RED) }
+            greenLedButton.setOnClickListener { switchLeds(AvailableLedColors.GREEN) }
+            blueLedButton.setOnClickListener { switchLeds(AvailableLedColors.BLUE) }
+            yellowLedButton.setOnClickListener { switchLeds(AvailableLedColors.YELLOW) }
+            whiteLedButton.setOnClickListener { switchLeds(AvailableLedColors.WHITE) }
+            purpleLedButton.setOnClickListener { switchLeds(AvailableLedColors.PURPLE) }
+            cyanLedButton.setOnClickListener { switchLeds(AvailableLedColors.CYAN) }
+
+            sendBrightnessButton.setOnClickListener { switchLedsBrightness() }
+        }
+    }
+
+    private fun switchLeds(color: AvailableLedColors) {
+        ledControllerViewModel.sendLedRequest(color)
+    }
+
+    private fun switchLedsBrightness() {
+        getBrightnessFromInput()?.let { brightness ->
+            getUrlFromInput()?.let { url ->
+                ledControllerViewModel.sendBrightnessRequest(brightness, url)
+            }
+        }
+    }
+
+    private fun getBrightnessFromInput(): Int? = try {
+        fragmentLedControllerBinding.brightnessField.text.toString().toInt()
+    } catch (e: Exception) {
+        Log.e(tag, "Exception while getting brightness from input")
+        onBrightnessInputError()
+        null
+    }
+
+    private fun onBrightnessInputError() {
+        onInputError("Wrong brightness input")
+    }
+
+
+    private fun getUrlFromInput(): String? =
+        try {
+            fragmentLedControllerBinding.ledNodeIpField.text.toString()
+        } catch (e: Exception) {
+            Log.e(tag, "Exception while converting input url", e)
+            onUrlInputError()
+            null
+        }
+
+    private fun onUrlInputError() {
+        onInputError("Wrong Url input")
+    }
+
+    @Suppress("SameParameterValue")
+    private fun onInputError(message: String) {
+        showShortToast(message)
     }
 }
---
+++ app/src/main/java/com/jj/androidenergyconsumer/viewmodels/LedControllerViewModel.kt
@@ -0,0 +1,15 @@
+package com.jj.androidenergyconsumer.viewmodels
+
+import androidx.lifecycle.ViewModel
+import com.jj.androidenergyconsumer.fragments.AvailableLedColors
+
+class LedControllerViewModel: ViewModel() {
+
+    fun sendLedRequest(color: AvailableLedColors) {
+        TODO("Not yet implemented")
+    }
+
+    fun sendBrightnessRequest(brightness: Int, url: String) {
+        TODO("Not yet implemented")
+    }
+}
