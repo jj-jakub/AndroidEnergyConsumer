8e42d2377ef347e508ecd57252a997cb9b8f4b5e
jj <>
2021-03-10 13:15:33 +0100

    Integrate with kotlinx-coroutines-test; fix disabled test

--- app/src/test/java/com/jj/androidenergyconsumer/gps/CustomLocationListenerTest.kt
+++ app/src/test/java/com/jj/androidenergyconsumer/gps/CustomLocationListenerTest.kt
@@ -1,32 +1,30 @@
 package com.jj.androidenergyconsumer.gps
 
 import android.location.Location
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runBlockingTest
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito
 
+@ExperimentalCoroutinesApi
 class CustomLocationListenerTest {
 
-    // TODO Fix test
-    @Disabled("Fix SharedFlow tests")
     @Test
-    fun `onLocationChanged should pass info by locationInfoUpdates`() = runBlocking {
+    fun `onLocationChanged should pass info by locationInfoUpdates`() = runBlockingTest {
         val customLocationListener = CustomLocationListener()
         val allLocationValues = mutableListOf<LocationListenerResult>()
 
-        val collectJob = launch {
-            customLocationListener.observeLocationInfoUpdates().collect {
-                println("Emitting")
-                allLocationValues.add(it)
-            }
+        val collectingJob = launch {
+            customLocationListener.observeLocationInfoUpdates().collect { allLocationValues.add(it) }
         }
 
         customLocationListener.onLocationChanged(Mockito.mock(Location::class.java))
         assertEquals(1, allLocationValues.size)
-        collectJob.cancel()
+        collectingJob.cancel()
     }
 }
