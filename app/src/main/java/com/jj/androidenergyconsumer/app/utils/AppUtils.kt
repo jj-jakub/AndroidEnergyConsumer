c96307e9a5556b3cd127386128a3e878edb32583
jj <>
2021-04-05 13:18:02 +0200

    Turn off saving logs to file

--- app/src/main/java/com/jj/androidenergyconsumer/app/utils/AppUtils.kt
+++ app/src/main/java/com/jj/androidenergyconsumer/app/utils/AppUtils.kt
@@ -9,7 +9,6 @@ import com.jj.androidenergyconsumer.app.AECApplication
 import com.jj.androidenergyconsumer.domain.coroutines.CoroutineScopeProvider
 import com.jj.androidenergyconsumer.domain.coroutines.ICoroutineScopeProvider
 import com.jj.androidenergyconsumer.domain.tag
-import kotlinx.coroutines.launch
 
 
 fun isAndroid6OrHigher() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
@@ -18,10 +17,10 @@ fun isAndroid8OrHigher() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.O
 fun logAndPingServer(message: String, tag: String,
                      coroutineScopeProvider: ICoroutineScopeProvider = CoroutineScopeProvider()) {
     Log.d(tag, message)
-    LogSaver.saveLog(tag, message)
-    coroutineScopeProvider.getIO().launch {
-//        PingDataCall.postSensorsData(PingData(Date(), "${Build.MODEL} $message"), DefaultCallback())
-    }
+//    LogSaver.saveLog(tag, message)
+//    coroutineScopeProvider.getIO().launch {
+////        PingDataCall.postSensorsData(PingData(Date(), "${Build.MODEL} $message"), DefaultCallback())
+//    }
 }
 
 fun showShortToast(message: String) =
