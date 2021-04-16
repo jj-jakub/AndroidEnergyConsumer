f8f4a6c61d0779f9320edc2952ec0d753508a3db
jj <>
2021-04-16 20:20:47 +0200

    Create MainActivityTest

---
+++ app/src/androidTest/java/com/jj/androidenergyconsumer/MainActivityTest.kt
@@ -0,0 +1,58 @@
+package com.jj.androidenergyconsumer
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.uiautomator.UiDevice
+import androidx.test.uiautomator.UiSelector
+import com.jj.androidenergyconsumer.app.activities.MainActivity
+import com.jj.androidenergyconsumer.app.utils.BatterySettingsLauncher
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.koin.java.KoinJavaComponent.inject
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+
+    companion object {
+        private const val ENERGY_OPTIMIZATION_DIALOG_TIMEOUT = 5000L
+        private const val ALLOW_STRING_REGEX = "(?i)\\bAllow\\b"
+    }
+
+    private lateinit var mDevice: UiDevice
+
+    private val batterySettingsLauncher: BatterySettingsLauncher by inject(BatterySettingsLauncher::class.java)
+
+    @get:Rule
+    var rule: ActivityScenarioRule<MainActivity> = ActivityScenarioRule(MainActivity::class.java)
+
+    @Before
+    fun setup() {
+        mDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
+        handleEnergyOptimizationDialog()
+    }
+
+    @Test
+    fun allFiveButtonsShouldBeVisible() {
+        onView(withId(R.id.calculationsButton)).check(matches(isDisplayed()))
+        onView(withId(R.id.gpsModuleLauncherButton)).check(matches(isDisplayed()))
+        onView(withId(R.id.internetModuleLauncherButton)).check(matches(isDisplayed()))
+        onView(withId(R.id.sensorsModuleLauncherButton)).check(matches(isDisplayed()))
+        onView(withId(R.id.bluetoothModuleLauncherButton)).check(matches(isDisplayed()))
+    }
+
+    private fun handleEnergyOptimizationDialog() {
+        if (batterySettingsLauncher.isAppIgnoringBatteryOptimizations()) {
+            mDevice.findObject(UiSelector().textMatches(ALLOW_STRING_REGEX)).apply {
+                waitForExists(ENERGY_OPTIMIZATION_DIALOG_TIMEOUT)
+                click()
+            }
+        }
+    }
+}
